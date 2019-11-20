44b56c81e97404a47cf44a9f38e6601b1afdf9a2
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2019-11-20 11:10:16 -0300

    Atualizando definição do MonoBehaviour;

--- FakeUnityEngine/MonoBehaviour.cs
+++ FakeUnityEngine/MonoBehaviour.cs
@@ -1,6 +1,17 @@
+using System.Collections;
+
 namespace UnityEngine {
     public abstract class MonoBehaviour {
+        public GameObject gameObject;
         public Transform transform;
         public bool enabled;
+
+        public void StartCoroutine(IEnumerator action) {
+
+        }
+
+        public void DontDestroyOnLoad(GameObject @object) {
+
+        }
     }
 }

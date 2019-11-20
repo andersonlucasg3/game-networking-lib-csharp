24978e60ac1a9c4a85a2357d20b85f104333470c
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2019-11-20 11:12:45 -0300

    Implementando novo delegate no INetworkClientDelegate;

--- GameServerLib/Sources/GameNetworking/Interfaces/INetworkingClientDelegate.cs
+++ GameServerLib/Sources/GameNetworking/Interfaces/INetworkingClientDelegate.cs
@@ -1,7 +1,10 @@
-﻿namespace GameNetworking.Networking {
+﻿using Messages.Models;
+
+namespace GameNetworking.Networking {
     public interface INetworkingClientDelegate {
         void NetworkingClientDidConnect();
         void NetworkingClientConnectDidTimeout();
         void NetworkingClientDidDisconnect();
+        void NetworkingClientDidReadMessage(MessageContainer container);
     }
 }

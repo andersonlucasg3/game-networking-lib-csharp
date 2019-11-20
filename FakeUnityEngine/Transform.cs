a03ee113b49c34b945c5a72a031d5287be17a262
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2019-11-20 17:15:15 -0300

    Corrigindo referências da API Unity;

--- FakeUnityEngine/Transform.cs
+++ FakeUnityEngine/Transform.cs
@@ -1,8 +1,8 @@
 namespace UnityEngine {
     public class Transform {
-        public Vector3 position;
-        public Quaternion rotation;
+        public Vector3 position { get; set; }
+        public Quaternion rotation { get; set; }
 
-        public Vector3 eulerAngles;
+        public Vector3 eulerAngles { get; set; }
     }
 }

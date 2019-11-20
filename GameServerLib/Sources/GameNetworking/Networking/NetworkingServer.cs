1df8383eb5bca650481a76a4abebcafba39dd0a9
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2019-11-20 11:16:29 -0300

    Reestruturando NetworkingServer para rodar utilizando threading;

--- GameServerLib/Sources/GameNetworking/Networking/NetworkingServer.cs
+++ GameServerLib/Sources/GameNetworking/Networking/NetworkingServer.cs
@@ -5,14 +5,18 @@ using Messages.Models;
 using System;
 using System.Collections.Generic;
 using Commons;
+using System.Threading;
 
 namespace GameNetworking.Networking {
     using Models;
 
     internal class NetworkingServer: WeakDelegate<INetworkingServerDelegate> {
         private readonly INetworking networking;
-
         private WeakReference weakMessagesDelegate;
+
+        private readonly List<NetworkClient> clientsStorage;
+
+        private Thread proletariatThread;
 
         public INetworkingServerMessagesDelegate MessagesDelegate {
             get { return this.weakMessagesDelegate?.Target as INetworkingServerMessagesDelegate; }
@@ -21,31 +25,21 @@ namespace GameNetworking.Networking {
 
         public NetworkingServer() {
             this.networking = new NetSocket();
+            clientsStorage = new List<NetworkClient>();
         }
 
         public void Listen(int port) {
             this.networking.Start(port);
+            CreateAndStartThread();
         }
 
-        public void AcceptClient() {
+        private void AcceptClient() {
             NetClient client = this.networking.Accept();
             if (client != null) {
                 NetworkClient networkClient = new NetworkClient(client, new MessageStreamReader(), new MessageStreamWriter());
+                clientsStorage.Add(networkClient);
                 this.Delegate?.NetworkingServerDidAcceptClient(networkClient);
             }
-        }
-
-        public void Read(NetworkClient client) {
-            byte[] bytes = this.networking.Read(client.Client);
-            client.Reader.Add(bytes);
-
-            MessageContainer message = null;
-            do {
-                message = client.Reader.Decode();
-                if (message != null) {
-                    this.MessagesDelegate?.NetworkingServerDidReadMessage(message, client);
-                }
-            } while (message != null);
         }
 
         public void Send(ITypedMessage encodable, NetworkClient client) {
@@ -58,12 +52,53 @@ namespace GameNetworking.Networking {
             clients.ForEach(c => this.networking.Send(c.Client, buffer));
         }
 
-        public void Flush(NetworkClient client) {
+        #region Private Methods
+
+        private void CreateAndStartThread() {
+            proletariatThread = new Thread(ThreadWork);
+            proletariatThread.Start();
+        }
+
+        private bool ShouldKeepWorking() {
+            // TODO:
+            // there is no way to stop a server yet
+            // when there is, add the check here
+            var isAborted = this.proletariatThread?.ThreadState == ThreadState.AbortRequested || this.proletariatThread?.ThreadState == ThreadState.Aborted;
+            return !isAborted;
+        }
+
+        private void ThreadWork() {
+            do {
+                this.AcceptClient();
+                this.clientsStorage.ForEach((each) => {
+                    this.Read(each);
+                    this.Flush(each);
+                });
+            } while (ShouldKeepWorking());
+        }
+
+        private void Read(NetworkClient client) {
+            byte[] bytes = this.networking.Read(client.Client);
+            client.Reader.Add(bytes);
+
+            MessageContainer message = null;
+            do {
+                message = client.Reader.Decode();
+                if (message != null) {
+                    this.MessagesDelegate?.NetworkingServerDidReadMessage(message, client);
+                }
+            } while (message != null);
+        }
+
+        private void Flush(NetworkClient client) {
             if (client.Client.IsConnected) {
                 this.networking.Flush(client.Client);
             } else {
                 this.Delegate?.NetworkingServerClientDidDisconnect(client);
+                this.clientsStorage.Remove(client);
             }
         }
+
+        #endregion
     }
 }

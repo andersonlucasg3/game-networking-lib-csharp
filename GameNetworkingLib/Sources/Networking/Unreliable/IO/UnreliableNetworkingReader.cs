1f46a8e7a89c87e6aaee693b5d6c6f455782d62d
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2020-03-22 01:29:25 -0300

    Implemented possible networking reader and writer for unreliable networking;

---
+++ GameNetworkingLib/Sources/Networking/Unreliable/IO/UnreliableNetworkingReader.cs
@@ -0,0 +1,28 @@
+﻿using Networking.Commons.IO;
+using Networking.Sockets;
+
+namespace Networking.IO {
+    public class UnreliableNetworkingReader : NetworkingReader<IUDPSocket> {
+        public interface IListener {
+            void ReaderDidRead(byte[] bytes, UDPSocket from);
+        }
+
+        private bool isReceiving = false;
+
+        public new IListener listener { get; set; }
+
+        public UnreliableNetworkingReader(IUDPSocket socket) : base(socket) { }
+
+        protected override void Receive() { 
+            if (this.isReceiving) { return; }
+
+            this.isReceiving = true;
+
+            this.socket.Read((bytes, fromSocket) => {
+                this.listener?.ReaderDidRead(bytes, fromSocket);
+
+                this.isReceiving = false;
+            });
+        }
+    }
+}
---
+++ GameNetworkingLib/Sources/Networking/Unreliable/IO/UnreliableNetworkingWriter.cs
@@ -0,0 +1,8 @@
+﻿using Networking.Commons.IO;
+using Networking.Sockets;
+
+namespace Networking.IO {
+    public class UnreliableNetworkingWriter : NetworkingWriter<IUDPSocket> {
+        public UnreliableNetworkingWriter(IUDPSocket socket) : base(socket) { }
+    }
+}

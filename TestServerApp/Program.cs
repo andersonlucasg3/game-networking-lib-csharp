3ed9d30abef5533d3a46e5b3bbd5233dbcd2e734
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2020-04-12 11:03:59 -0300

    Added pile of actions to the server;

--- TestServerApp/Program.cs
+++ TestServerApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using GameNetworking;
 using GameNetworking.Commons;
@@ -9,18 +10,25 @@ using UnreliablePlayer = GameNetworking.Commons.Models.Server.NetworkPlayer<Netw
 
 namespace TestServerApp {
     class Program : IMainThreadDispatcher {
+        private readonly List<Action> actions = new List<Action>();
+
         static void Main(string[] _) {
-            UnreliableGameServer<UnreliablePlayer> server = new UnreliableGameServer<UnreliablePlayer>(new UnreliableNetworkingServer(new UnreliableSocket(new UDPSocket())), new Program());
+            var program = new Program();
+            UnreliableGameServer<UnreliablePlayer> server = new UnreliableGameServer<UnreliablePlayer>(new UnreliableNetworkingServer(new UnreliableSocket(new UDPSocket())), program);
 
             server.Start(IPAddress.Any.ToString(), 64000);
 
             while (true) {
+                var copyActions = new List<Action>(program.actions);
+                program.actions.RemoveAll(_ => true);
+                copyActions.ForEach(a => a.Invoke());
+
                 server.Update();
             }
         }
 
         public void Enqueue(Action action) {
-            action.Invoke();
+            this.actions.Add(action);
         }
     }
 }

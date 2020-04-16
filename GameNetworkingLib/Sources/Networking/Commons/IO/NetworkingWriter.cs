e9bf14f037ee9fe1f5ccf603692a163461b279b5
Anderson Lucas C. Ramos <anderson@unknown.home>
2020-04-16 18:18:29 -0300

    Changed ConcurrentBag to ConcurrentQueue;

--- GameNetworkingLib/Sources/Networking/Commons/IO/NetworkingWriter.cs
+++ GameNetworkingLib/Sources/Networking/Commons/IO/NetworkingWriter.cs
@@ -10,20 +10,20 @@ namespace Networking.Commons.IO {
     public abstract class NetworkingWriter<TSocket> : IWriter
         where TSocket : ISocket {
         private readonly TSocket socket;
-        private readonly ConcurrentBag<byte[]> buffer;
+        private readonly ConcurrentQueue<byte[]> buffer;
 
         private bool isSending = false;
 
         internal NetworkingWriter(TSocket socket) {
             this.socket = socket;
-            this.buffer = new ConcurrentBag<byte[]>();
+            this.buffer = new ConcurrentQueue<byte[]>();
         }
 
         private void Write() {
             if (this.isSending) { return; }
             this.isSending = true;
 
-            if (!this.buffer.TryTake(out byte[] buffer)) { return; }
+            if (!this.buffer.TryDequeue(out byte[] buffer)) { return; }
 
             this.socket.Write(buffer, (written) => {
                 this.isSending = false;
@@ -31,7 +31,7 @@ namespace Networking.Commons.IO {
         }
 
         public void Write(byte[] data) {
-            this.buffer.Add(data);
+            this.buffer.Enqueue(data);
             this.Write();
         }
 

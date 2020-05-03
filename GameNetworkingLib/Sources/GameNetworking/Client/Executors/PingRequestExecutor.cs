54de718ac4e177e865e9fc192ccbe752223c50dd
Anderson Lucas de Castro Ramos <anderson@imacdeanderson.home>
2020-05-03 01:29:25 -0300

    Fixed nullpointer on router;

--- GameNetworkingLib/Sources/GameNetworking/Client/Executors/PingRequestExecutor.cs
+++ GameNetworkingLib/Sources/GameNetworking/Client/Executors/PingRequestExecutor.cs
@@ -7,7 +7,7 @@ using GameNetworking.Messages.Server;
 namespace GameNetworking.Executors.Client {
     internal class PingRequestExecutor<TPlayer> : Commons.BaseExecutor<IGameClient<TPlayer>, PingRequestMessage>
         where TPlayer : GameNetworking.Client.Player {
-        public PingRequestExecutor(IGameClient<TPlayer> client) : base(client, null) { }
+        public PingRequestExecutor(IGameClient<TPlayer> client, PingRequestMessage message) : base(client, message) { }
 
         public override void Execute() {
             if (this.instance.localPlayer != null) {
--- GameNetworkingLib/Sources/GameNetworking/Client/GameClientMessageRouter.cs
+++ GameNetworkingLib/Sources/GameNetworking/Client/GameClientMessageRouter.cs
@@ -11,7 +11,7 @@ namespace GameNetworking.Commons.Client {
         void Route(NetEndPoint from, MessageContainer container);
     }
 
-    public class GameClientMessageRouter<TPlayer>: IClientMessageRouter
+    public class GameClientMessageRouter<TPlayer> : IClientMessageRouter
         where TPlayer : GameNetworking.Client.Player {
 
         private readonly IMainThreadDispatcher dispatcher;
@@ -30,7 +30,7 @@ namespace GameNetworking.Commons.Client {
 
             switch ((MessageType)container.type) {
                 case MessageType.connectedPlayer: this.Execute(new ConnectedPlayerExecutor(this.game as IRemoteClientListener, container.Parse<ConnectedPlayerMessage>())); break;
-                case MessageType.ping: this.Execute(new PingRequestExecutor<TPlayer>(this.game)); break;
+                case MessageType.ping: this.Execute(new PingRequestExecutor<TPlayer>(this.game, container.Parse<PingRequestMessage>())); break;
                 case MessageType.pingResult: this.Execute(new PingResultRequestExecutor<TPlayer>(this.game, container.Parse<PingResultRequestMessage>())); break;
                 case MessageType.disconnectedPlayer: this.Execute(new DisconnectedPlayerExecutor(this.game as IRemoteClientListener, container.Parse<DisconnectedPlayerMessage>())); break;
                 default: this.game?.listener?.GameClientDidReceiveMessage(container); break;
